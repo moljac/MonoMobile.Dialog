8a433afee103579c88c31bee04c6622013811e3d
moljac <mcvjetko@holisticware.net>
2012-11-29 12:37:15 +0100

    API Normalization from MA.D added in MT.D
    Kevin had 2 ctors with diferent signature - added in MT.D
    CheckBoxElement and FloatElement

---
+++ MonoMobile.Dialog.Touch/NormalizationExtensions/FloatElement.MA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.UIKit;
+using MonoTouch.CoreGraphics;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoMobile.Dialog.Utilities;
+using MonoTouch.CoreAnimation;
+
+namespace MonoMobile.Dialog
+{
+	/// <summary>
+	///  Used to display a slider on the screen.
+	/// </summary>
+	public partial class FloatElement 
+	{
+
+		# region    Constructors
+		//-------------------------------------------------------------------------
+		public FloatElement(string caption)
+			: base(caption)
+		{
+		}
+		//-------------------------------------------------------------------------
+		# endregion Constructors
+
+	}
+}
