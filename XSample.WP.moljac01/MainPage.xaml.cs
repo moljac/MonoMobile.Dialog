4eae7d7824b7ec8f2a6c53e28d2cd3b83ad1f4fa
ikxx <ivan.kaselj@gmail.com>
2012-11-30 11:40:16 +0100

    Fix ScrollViewer (layout) problems

--- XSample.WP.moljac01/MainPage.xaml.cs
+++ XSample.WP.moljac01/MainPage.xaml.cs
@@ -51,27 +51,27 @@ namespace XSample.WP.moljac01
 	
 	
 			RootElement re = 
-				new RootElement("Caption")
+			new RootElement("Root")
+			{
+				new Section ("- Section 00") 
 				{
-				  new Section ("Section 00") 
+					new EntryElement ("- - Entry Eelment", "placeholder", "")
+					, new RootElement ("- - Root 2") 
 					{
-					  new EntryElement ("caption", "placeholder", "")
-					, new RootElement ("Root 2") 
+						new Section ("- - - Section") 
 						{
-						  new Section ("Section") 
-							{
-							  new EntryElement ("caption", "placeholder", "")
-							, new StringElement ("Back", () => {})
-							}
+							new EntryElement ("- - - - EntryElement", "placeholder", "")
+							, new StringElement ("- - - - String element Back", () => {})
 						}
 					}
-				, new Section("Section 01", "Footer")
-					{
-					  new StringElement("StringElement caption")
-					, new StringElement("StringElement caption", "value")
-					, new EntryElement("EntryElement caption", "value")
-					, new EntryElement("EntryElement caption", "hint", "value")
-					, new EntryElement("EntryElement caption", "placeholder", "value", true)
+				}
+				, new Section("- Section 01", "Footer")
+				{
+					new StringElement("- - StringElement caption")
+					, new StringElement("- - StringElement caption", "value value value value value value value value value value value value value value value value value value value value value value value value value value value value value value value value value ")
+					, new EntryElement("- - EntryElement caption", "value")
+					, new EntryElement("- - EntryElement caption", "hint", "value")
+					, new EntryElement("- - EntryElement caption", "placeholder", "value", true)
 					//, new BooleanElement("BooleanElement caption", false)
 
 					// Exception:
@@ -79,42 +79,44 @@ namespace XSample.WP.moljac01
 					// A first chance exception of type 'System.IO.FileNotFoundException' 
 					// occurred in mscorlib.dll
 					//, new BooleanElement("BooleanElement caption", false, "key")
-					, new CheckboxElement("CheckboxElement caption")
-					, new CheckboxElement("CheckboxElement caption", false)
-					, new CheckboxElement("CheckboxElement caption", true, "group")
-					, new CheckboxElement("CheckboxElement caption", true, "subcaption", "group")
-					, new DateElement("DateElement caption", DateTime.Now)
+					, new CheckboxElement("- - CheckboxElement caption")
+					, new CheckboxElement("- - CheckboxElement caption", false)
+					, new CheckboxElement("- - CheckboxElement caption", true, "group")
+					, new CheckboxElement("- - CheckboxElement caption", true, "subcaption", "group")
+					, new DateElement("- - DateElement caption", DateTime.Now)
 					//, new DateElement("DateElement1 caption", DateTime.Now)
 					//, new DateElement("DateElement2 caption", DateTime.Now)
 					//, new DateElement("DateElement3 caption", DateTime.Now)
 					//, new DateElement("DateElement4 caption", DateTime.Now)
 					//, new DateElement("DateElement5 caption", DateTime.Now)
-					, new DateTimeElement("DateTimeElement caption", DateTime.Now)
-					, new DateTimeElement("DateTimeElement1 caption", DateTime.Now)
-					, new FloatElement("")
-					, new FloatElement(0.4f, 0.0f, 10f)
-					, new ImageElement(null)
-					, new MultilineElement("MultilineElement caption")
-					, new MultilineElement("MultilineElement caption", delegate(){})
-					, new MultilineElement("MultilineElement caption", "value")
+					, new DateTimeElement("- - DateTimeElement caption", DateTime.Now)
+					, new DateTimeElement("- - DateTimeElement1 caption", DateTime.Now)
+					//, new FloatElement("- - Float")
+					//, new FloatElement(0.4f, 0.0f, 10f)
+					//, new ImageElement(null)
+					, new MultilineElement("- - MultilineElement caption")
+					, new MultilineElement("- - MultilineElement caption", delegate(){})
+					, new MultilineElement("- - MultilineElement caption", "value")
 					//, new HtmlElement("HtmlElement caption", new NSUrl("http://holisiticware.net"))
-					}
+				}
 				//, new Section("Section 02", "Footer")
 				//	{
 				//	}
-				};
+			};
 
 			DialogViewController dvc = 
 				new DialogViewController(re)
 				// new DialogViewController(e, true)
 				;
 
-			// LayoutRoot.Children.Add(dvc);
-			ContentPanel.Children.Add(dvc);
+			// Create the final scrooling environment
+			ScrollViewer scrollViewer = new ScrollViewer()
+			{
+				Content = dvc
+			};
 
 
-
-
+			ContentPanel.Children.Add(scrollViewer);
 		}
 	}
 }
